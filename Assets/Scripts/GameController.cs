6496f1021c7027f0eff706edd2e5b7d9fa52154d
Gust-V <gustavoh.venturavicente10@gmail.com>
2021-03-18 22:15:35 -0300

    MenusPrototipo

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public int touch;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        touch = Input.touchCount;
     }
 }
---
+++ Assets/Scripts/sButtom.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+
+public class sButtom : MonoBehaviour
+{
+    public void Touch()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        if (currentSceneName == "ScHome")
+        {
+            string nameButton = EventSystem.current.currentSelectedGameObject.name;
+
+
+            if (nameButton == "Bt00_Qt")
+            {
+                SceneManager.LoadScene("ScQuarto");
+            }
+            else if (nameButton == "Bt01_Sl")
+            {
+                SceneManager.LoadScene("ScSala");
+            }
+            else if(nameButton == "Bt02_Bn")
+            {
+                SceneManager.LoadScene("ScBanheiro");
+            }
+            else if (nameButton == "Bt03_Cz")
+            {
+                SceneManager.LoadScene("ScCozinha");
+            }
+        }
+        else if (currentSceneName != "ScHome")
+        {
+            SceneManager.LoadScene("ScHome");
+        }
+
+    }
+}
+
